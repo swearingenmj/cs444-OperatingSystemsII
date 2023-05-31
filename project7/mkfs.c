edb0df9e729ade2471370b56cc39981889b8fce4
Melissa Swearingen <swearingenmj@gmail.com>
2023-05-30 20:48:29 -0700

    clean up code, remove excessive comments

--- project7/mkfs.c
+++ project7/mkfs.c
@@ -18,21 +18,15 @@ void mkfs(void){
         alloc();
     }
 
-    // 1. call ialloc to get new inode
     struct inode *new_inode = ialloc();
-
-    // 2. call alloc to get new data block
     int new_data_block = alloc();
 
-    // 3. initialize inode returned from step 1.
     new_inode->flags = FLAG_INIT;
     new_inode->size = ENTRY_SIZE;
     new_inode->block_ptr[0] = new_data_block;
 
-    // 4. make an unsigned char block[BLOCK_SIZE] to populate with new directory info
     unsigned char block[BLOCK_SIZE] = {0};
 
-    // 5. Add directory entries. 
     char *dot = (char *)(block);
     char *double_dot = (char *)(block + ENTRY_SIZE);
 
@@ -42,9 +36,7 @@ void mkfs(void){
     strcpy(dot + INODE_SIZE, ".");
     strcpy(double_dot + INODE_SIZE + ENTRY_SIZE, "..");
     
-    // 6. write the directory data block with bwrite
     bwrite(new_data_block, block);
 
-    // 7. call iput to write inode to disk and clear incore 
     iput(new_inode);
 }

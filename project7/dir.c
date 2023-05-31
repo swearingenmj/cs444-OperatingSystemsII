ebe4a482d69b76a433797fcafb4541dbb4546bd7
SamsonDeVol <devols@oregonstate.edu>
2023-05-31 09:15:12 -0700

    remove comments from dir.c

--- project7/dir.c
+++ project7/dir.c
@@ -32,21 +32,16 @@ int directory_get(struct directory *dir, struct directory_entry *ent) {
     int data_block_index = dir->offset / 4096;
     int data_block_num = dir->inode->block_ptr[data_block_index];
     
-    // printf("__data block num: %d\n", data_block_num);
-    
     unsigned char block[BLOCK_SIZE] = {0};
     bread(data_block_num, block);
 
     int offset_in_block = dir->offset % 4096;
-    // printf("__offset in block: %d\n", offset_in_block);
-    // printf("__read into block: %d\n", block[offset_in_block + INODE_NUM_SIZE]);
     int inode_num = read_u16(block + offset_in_block);
+    
     ent->inode_num = inode_num;
-    // printf("__inode num: %d\n", inode_num);
 
     char* read_block_name_ptr = (char*)block + offset_in_block + INODE_NUM_SIZE;
     strcpy(ent->name, read_block_name_ptr);
-    // printf("__name: %s\n", read_block_name_ptr);
 
     dir->offset+=32;
     return 0;
